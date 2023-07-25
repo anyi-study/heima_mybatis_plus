d46fbc22db53db7de07cb6b6f1dcdcd96647c2d9
laoou <jx523644@163.com>
2023-07-25 21:22:10 +0800

    普通sql方法，增删改查

--- mybatis_01/src/test/java/com/laoou/Mybatis01ApplicationTests.java
+++ mybatis_01/src/test/java/com/laoou/Mybatis01ApplicationTests.java
@@ -14,6 +14,43 @@ class Mybatis01ApplicationTests {
     @Autowired
     private UserDao userDao;
 
+
+
+//    根据ID查询
+    @Test
+    void testGetById(){
+        User user = userDao.selectById("1");
+        System.out.println(user);
+    }
+//    更新
+    @Test
+    void testUpdate(){
+        User user = new User();
+        user.setId(1L);
+        user.setName("老罗666");
+        user.setPassword("123456");
+
+        userDao.updateById(user);
+    }
+
+
+//    删除
+    @Test
+    void testDelete(){
+        userDao.deleteById("1683827284185698305");
+    }
+
+//    新增
+    @Test
+    void testSave(){
+        User user = new User();
+        user.setName("老罗");
+        user.setPassword("laoou");
+        user.setAge(12);
+        user.setTel("1231");
+        userDao.insert(user);
+    }
+//    查询全部
     @Test
     void testGetAll() {
         List<User> users = userDao.selectList(null);
